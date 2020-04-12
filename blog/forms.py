87d655294f49ae61e699a461a3536d67de608e6c
srikant <srikantkanekar01@gmail.com>
2020-04-12 13:16:50 +0530

    authentication

--- blog/forms.py
+++ blog/forms.py
@@ -36,10 +36,8 @@ class UserLoginForm(forms.Form):
             user = authenticate(username=username, password=password)
             if not user:
                 raise forms.ValidationError('This user does not exist')
-            if not user.check_password(password):
+            elif not user.check_password(password):
                 raise forms.ValidationError('Incorrect Password')
-            if not user.is_active:
-                raise forms.ValidationError('User is no longer active')
 
         return super(UserLoginForm, self).clean(*args, **keyargs)
 
@@ -53,13 +51,13 @@ class UsersRegisterForm(forms.ModelForm):
         fields = [
             "username",
             "email",
-            "confirm_email",
             "password",
+            "confirm_password",
         ]
     username = forms.CharField()
     email = forms.EmailField(label="Email")
-    confirm_email = forms.EmailField(label="Confirm Email")
     password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
 
     def __init__(self, *args, **kwargs):
         super(UsersRegisterForm, self).__init__(*args, **kwargs)
@@ -69,21 +67,18 @@ class UsersRegisterForm(forms.ModelForm):
         self.fields['email'].widget.attrs.update({
             'class': 'form-control',
             "name": "email"})
-        self.fields['confirm_email'].widget.attrs.update({
-            'class': 'form-control',
-            "name": "confirm_email"})
         self.fields['password'].widget.attrs.update({
             'class': 'form-control',
             "name": "password"})
+        self.fields['confirm_password'].widget.attrs.update({
+            'class': 'form-control',
+            "name": "confirm_password"})
 
     def clean(self, *args, **keyargs):
         email = self.cleaned_data.get("email")
-        confirm_email = self.cleaned_data.get("confirm_email")
         username = self.cleaned_data.get("username")
         password = self.cleaned_data.get("password")
-
-        if email != confirm_email:
-            raise forms.ValidationError("Email must match")
+        confirm_password = self.cleaned_data.get("confirm_password")
 
         email_qs = User.objects.filter(email=email)
         if email_qs.exists():
@@ -93,10 +88,11 @@ class UsersRegisterForm(forms.ModelForm):
         if username_qs.exists():
             raise forms.ValidationError("User with this username already registered")
 
-        # you can add more validations for password
-
         if len(password) < 8:
-            raise forms.ValidationError("Password must be greater than 8 characters")
+            raise forms.ValidationError("Password must have at least 8 characters")
+
+        if password != confirm_password:
+            raise forms.ValidationError("Passwords are not matching")
 
         return super(UsersRegisterForm, self).clean(*args, **keyargs)
 
