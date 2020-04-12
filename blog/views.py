7ad91d3848c17f5f42610f852c3e0b04a133875f
srikant <srikantkanekar01@gmail.com>
2020-04-12 11:38:43 +0530

    authentication

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
-from .forms import PostForm, CommentForm
+from .forms import PostForm, CommentForm, UserLoginForm, UsersRegisterForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
 
 
 def post_list(request):
@@ -76,3 +77,33 @@ def add_comment_to_post(request, pk):
     else:
         form = CommentForm()
         return render(request, 'blog/add_comment_to_post.html', {'form': form})
+
+
+def login_view(request):
+    form = UserLoginForm(request.POST or None)
+    if form.is_valid():
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        return redirect("/")
+    return render(request, "registration/login.html", {
+        'form': form,
+        'title': 'login',
+    })
+
+
+def register_view(request):
+    form = UsersRegisterForm(request.POST or None)
+    if form.is_valid():
+        user = form.save()
+        password = form.cleaned_data.get("password")
+        user.set_password(password)
+        user.save()
+        new_user = authenticate(username=user.username, password=password)
+        login(request, new_user)
+        return redirect("/")
+    return render(request, "registration/login.html", {
+        "title": "register",
+        "form": form,
+    })
